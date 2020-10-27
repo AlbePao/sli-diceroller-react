10178c8484457db200c344886652d2d62395561c
Alberto Paolucci <alberto.paolucci@icloud.com>
2020-10-27 19:12:15 +0100

    Updated Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -2,17 +2,15 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 import 'firebase/auth';
 
-const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-};
-
-firebase.initializeApp(config);
+firebase.initializeApp({
+  apiKey: 'AIzaSyCKAH7MAk8ZL86c1ilSUQOMMDCQK4DeOzU',
+  authDomain: 'sli-diceroller-ef530.firebaseapp.com',
+  databaseURL: 'https://sli-diceroller-ef530.firebaseio.com',
+  projectId: 'sli-diceroller-ef530',
+  storageBucket: 'sli-diceroller-ef530.appspot.com',
+  messagingSenderId: '606933124052',
+  appId: '1:606933124052:web:957ab0fbc2cb252fb88c41',
+  measurementId: 'G-BXMXH7CTWR',
+});
 
 export default firebase;
